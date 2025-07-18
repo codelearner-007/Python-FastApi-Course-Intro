6147fd887739b131ff27430cbfca20ce98a8e444
codelearner-007 <farhanahmad0819@gmail.com>
2025-07-18 19:44:30 +0500

    lecture #10 (post request with making a dict)

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get("/")
+@app.get("/posts")
 def get_post():
     return {'data': "This is your posts"}
+
+
+@app.post("/createposts")
+def create_post(payload: dict = Body(...)):
+    print("payload", payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
