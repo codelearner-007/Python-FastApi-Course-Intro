788e4806ab20befb3c832a7c0de9acd45116dc4f
codelearner-007 <farhanahmad0819@gmail.com>
2025-07-21 19:50:36 +0500

    Lecture #15 (Get Post by ID)
    In it we get the post using "http://127.0.0.1:8000/posts/1"
    this url and in code
    first we get the get the id "@app.get("/posts/{id}")"
    validate it as integer like
    "def get_post(id: int):"
    and then find the post using
    "def find_post(id):" function and return it

--- main.py
+++ main.py
@@ -15,12 +15,19 @@ class Post(BaseModel):
     published: bool = True  # It comes with default condition as "True"
     rating: Optional[int] = None
     # Optional as if it is present, it only be a integer other wise it will be none
-    id: Optional[int] = None
+    # id: Optional[int] = None
 
 
 # This we make a my_post data array for every now post we make from front end
 my_post = [{"title": "title of post 1",
             "content": "content of post 1", "id": 1}, {"title": "favirot food", "content": "I like pizza", "id": 2}]
+
+
+# This is the "find product" function having argument of "id"
+def find_post(id):
+    for p in my_post:
+        if p["id"] == id:
+            return p
 
 
 @app.get("/")
@@ -33,16 +40,35 @@ def get_post():
     return {'data': my_post}
 
 
-@app.post("/posts")
-def create_posts(post: Post):
-    # post_dict = post.dict()
-    post.id = randrange(0, 10000000)
-    my_post.append(post)
-    return {'data': post}
-
+# //////////////////////////////////////////////
+# These are same code with a differenet thing that if we have a id field in the model the then we just access it using "." and the assign a random value
 # @app.post("/posts")
 # def create_posts(post: Post):
-#     post_dict = post.dict()
-#     post_dict["id"] = randrange(0, 10000000)
-#     my_post.append(post_dict)
-#     return {'data': post_dict}
+#     post.id = randrange(0, 10000000)
+#     my_post.append(post)
+# #     return {'data': post}
+#     return {'Message ': "New post created"}
+
+# In that case the model do not have the "id" in it and we create the id for the every post which we create
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 10000000)
+    my_post.append(post_dict)
+    # return {'data': post_dict}
+    return {'Message ': "New post created"}
+
+
+# ///////////////////////////////////////////
+# Get Post by ID:
+@app.get("/posts/{id}")  # This is for the URL
+def get_post(id: int):  # we get the id is parameter like "2" and validate it as string now if the id is not a integer entered by the user thn it shows that the id is not a integer other wise it gives the generic error
+
+    # We find the product using the "id" received and save it into a new variable "post"
+    print(type(id))  # Here in print it show that type of id is a string type
+    post = find_post(id)  # NOW HERE THE type is integer
+    return {"post Detail":  post}
+
+# Special NOte
+# Any time we have a path parameter like "@app.get("/posts/{id}")"  its result is always be a string like in this case there is a "2" but this is a string type
+# we have to manually convert it into a integer so that we can get the 2 post
