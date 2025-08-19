e89371b59259de66d06d8d7b46c8d16a2e89d90b
codelearner-007 <farhanahmad0819@gmail.com>
2025-08-19 17:02:03 +0500

    Lecture #42 (Till That) Delete Post
    In it we practice how to connect the delete function with the pgAdmin 4

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from fastapi.params import Body
 from pydantic import BaseModel  # This is for importing the base model
 from typing import Optional
 from random import randrange  # This is for the random number used in id
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
@@ -13,10 +16,19 @@ class Post(BaseModel):
     title: str  # it define the types of data received from front end
     content: str
     published: bool = True  # It comes with default condition as "True"
-    rating: Optional[int] = None
-    # Optional as if it is present, it only be a integer other wise it will be none
-    # id: Optional[int] = None
 
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host='localhost', database='fastapi', user='postgres', password='090078601@Ab', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connection to database failed")
+        print("Error", error)
+        time.sleep(2)
 
 # This we make a my_post data array for every now post we make from front end
 my_post = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
@@ -42,20 +54,25 @@ def find_index_post(id):
 def home():
     return {"message": "Hello World"}
 
+# Here we get all the post available in the array
+
 
 @app.get("/posts")
 def get_post():
-    return {'data': my_post}
+    cursor.execute(""" SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    print(posts)
+    return {'data': posts}
 
 
 # In that case the model do not have the "id" in it and we create the id for the every post which we create
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0, 10000000)
-    my_post.append(post_dict)
-    # return {'data': post_dict}
-    return {'Message ': "New post created"}
+    cursor.execute(""" INSERT INTO posts (title,content,published) VALUES (%s , %s ,%s) RETURNING * """,
+                   (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {'data': new_post}
 
 
 @app.get("/posts/latest")
@@ -67,11 +84,9 @@ def get_latest_post():
 # ///////////////////////////////////////////
 # Get Post by ID:
 @app.get("/posts/{id}")  # This is for the URL
-def get_post(id: int):  # we get the id is parameter like "2" and validate it as string now if the id is not a integer entered by the user thn it shows that the id is not a integer other wise it gives the generic error
-
-    # We find the product using the "id" received and save it into a new variable "post"
-    print(type(id))  # Here in print it show that type of id is a string type
-    post = find_post(id)  # NOW HERE THE type is integer
+def get_post(id: int):  # we get the id is parameter like "2" and validate it as string now if the id is not a integer entered by the user then it shows that the id is not a integer other wise it gives the generic error
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id)))
+    post = cursor.fetchone()
 
     # Lets suppose We don't have the post which the user want then
     if not post:
@@ -84,17 +99,26 @@ def get_post(id: int):  # we get the id is parameter like "2" and validate it as
 # we have to manually convert it into a integer so that we can get the 2 post
 
 
+# ---- This is the delete case -----#
+# In this we find the index of the post which we want to delete. If index not found then it means that the post is not there. but if found the my_post.pop(index) will remove the post
+
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     # deleting post
-    index = find_index_post(id)
-    if index == None:
+
+    cursor.execute(
+        """ DELETE FROM posts WHERE id = %s returning * """, (str(id),))
+    delete_post = cursor.fetchone()
+    conn.commit()
+
+    if delete_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
-    my_post.pop(index)
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
+# ---- This is the Update case -----#
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
     index = find_index_post(id)
