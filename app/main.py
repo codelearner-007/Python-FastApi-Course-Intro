774af73df5f003d90f83bb5cf80bcb5833d68082
codelearner-007 <farhanahmad0819@gmail.com>
2025-08-21 16:52:39 +0500

    Lecture #58 Get User By Id

--- app/main.py
+++ app/main.py
@@ -151,7 +151,7 @@ def update_post(id: int, updated_post: schemas.PostCreate , db: Session = Depend
 # .................................................................................
 
 # .................................................................................
-# .......................... post method for post .............................
+# .......................... post method for User .............................
 
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
@@ -169,3 +169,14 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     
     return new_user
 
+# .................................................................................
+# .......................... get method for User .............................
+
+@app.get('/users/{id}', response_model=schemas.UserOut)
+def get_user(id: int , db : Session = Depends(get_db)):
+    user =  db.query(models.User).filter(models.User.id == id).first()
+   
+    if not user:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail=f"User with id {id} does not exist")
+    
+    return user
